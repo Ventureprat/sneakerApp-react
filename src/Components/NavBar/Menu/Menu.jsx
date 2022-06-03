85423703cba71a3a1750a98cadf93b15fe2da2d8
username <pratyushhota1999@gmail.com>
2022-06-03 23:32:52 +0530

    mobile 376px dev complete

--- src/Components/NavBar/Menu/Menu.jsx
+++ src/Components/NavBar/Menu/Menu.jsx
@@ -15,32 +15,56 @@ export default function Menu({ menuVisibility, visibilityFunc }) {
         <>
           <ul className="menu-linkList">
             <li className="menu-links">
-              <Link to="/" style={{ textDecoration: "none" }}>
+              <Link
+                to="/"
+                style={{ textDecoration: "none" }}
+                className="menu-link"
+              >
                 Home
               </Link>
             </li>
             <li className="menu-links">
-              <Link to="/Collections" style={{ textDecoration: "none" }}>
+              <Link
+                to="/Collections"
+                style={{ textDecoration: "none" }}
+                className="menu-link"
+              >
                 Collections
               </Link>
             </li>
             <li className="menu-links">
-              <Link to="/Men" style={{ textDecoration: "none" }}>
+              <Link
+                to="/Men"
+                style={{ textDecoration: "none" }}
+                className="menu-link"
+              >
                 Men
               </Link>
             </li>
             <li className="menu-links">
-              <Link to="/Women" style={{ textDecoration: "none" }}>
+              <Link
+                to="/Women"
+                style={{ textDecoration: "none" }}
+                className="menu-link"
+              >
                 Women
               </Link>
             </li>
             <li className="menu-links">
-              <Link to="/About" style={{ textDecoration: "none" }}>
+              <Link
+                to="/About"
+                style={{ textDecoration: "none" }}
+                className="menu-link"
+              >
                 About
               </Link>
             </li>
             <li className="menu-links">
-              <Link to="/Contact" style={{ textDecoration: "none" }}>
+              <Link
+                to="/Contact"
+                style={{ textDecoration: "none" }}
+                className="menu-link"
+              >
                 Contact
               </Link>
             </li>
